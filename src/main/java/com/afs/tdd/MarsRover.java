9c53472ce3f801b1799dbfcc147e037002c49c1d
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-17 22:17:14 +0800

    feat: change executeCommand to executeBatchCommands and accept Command List

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,5 +1,7 @@
 package com.afs.tdd;
 
+import java.util.List;
+
 public class MarsRover {
 
     private final Location location;
@@ -8,36 +10,38 @@ public class MarsRover {
         this.location = location;
     }
 
-    public void executeCommand(Command givenCommand) {
-        if(givenCommand == Command.MOVE) {
-            if(location.getDirection() == Direction.NORTH){
-                location.setY(location.getY() + 1);
-            } else if(location.getDirection() == Direction.SOUTH){
-                location.setY(location.getY() - 1);
-            } else if(location.getDirection() == Direction.EAST){
-                location.setX(location.getX() + 1);
-            } else if(location.getDirection() == Direction.WEST){
-                location.setX(location.getX() - 1);
-            }
-        } else if (givenCommand == Command.TURN_LEFT) {
-            if(location.getDirection() == Direction.NORTH){
-                location.setDirection(Direction.WEST);
-            } else if(location.getDirection() == Direction.SOUTH){
-                location.setDirection(Direction.EAST);
-            } else if(location.getDirection() == Direction.EAST){
-                location.setDirection(Direction.NORTH);
-            } else if(location.getDirection() == Direction.WEST){
-                location.setDirection(Direction.SOUTH);
-            }
-        } else if (givenCommand == Command.TURN_RIGHT) {
-            if(location.getDirection() == Direction.NORTH){
-                location.setDirection(Direction.EAST);
-            } else if(location.getDirection() == Direction.SOUTH){
-                location.setDirection(Direction.WEST);
-            } else if(location.getDirection() == Direction.EAST){
-                location.setDirection(Direction.SOUTH);
-            } else if(location.getDirection() == Direction.WEST){
-                location.setDirection(Direction.NORTH);
+    public void executeBatchCommands(List<Command> givenCommands) {
+        for (Command givenCommand : givenCommands) {
+            if (givenCommand == Command.MOVE) {
+                if (location.getDirection() == Direction.NORTH) {
+                    location.setY(location.getY() + 1);
+                } else if (location.getDirection() == Direction.SOUTH) {
+                    location.setY(location.getY() - 1);
+                } else if (location.getDirection() == Direction.EAST) {
+                    location.setX(location.getX() + 1);
+                } else if (location.getDirection() == Direction.WEST) {
+                    location.setX(location.getX() - 1);
+                }
+            } else if (givenCommand == Command.TURN_LEFT) {
+                if (location.getDirection() == Direction.NORTH) {
+                    location.setDirection(Direction.WEST);
+                } else if (location.getDirection() == Direction.SOUTH) {
+                    location.setDirection(Direction.EAST);
+                } else if (location.getDirection() == Direction.EAST) {
+                    location.setDirection(Direction.NORTH);
+                } else if (location.getDirection() == Direction.WEST) {
+                    location.setDirection(Direction.SOUTH);
+                }
+            } else if (givenCommand == Command.TURN_RIGHT) {
+                if (location.getDirection() == Direction.NORTH) {
+                    location.setDirection(Direction.EAST);
+                } else if (location.getDirection() == Direction.SOUTH) {
+                    location.setDirection(Direction.WEST);
+                } else if (location.getDirection() == Direction.EAST) {
+                    location.setDirection(Direction.SOUTH);
+                } else if (location.getDirection() == Direction.WEST) {
+                    location.setDirection(Direction.NORTH);
+                }
             }
         }
     }
