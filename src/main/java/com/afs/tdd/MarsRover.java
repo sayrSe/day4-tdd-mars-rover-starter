c83824e338bb5a99cd1407103443115d47887daa
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-17 22:42:47 +0800

    refactor: extract move, turnLeft and turnRight methods

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -13,36 +13,48 @@ public class MarsRover {
     public void executeBatchCommands(List<Command> givenCommands) {
         for (Command givenCommand : givenCommands) {
             if (givenCommand == Command.MOVE) {
-                if (location.getDirection() == Direction.NORTH) {
-                    location.setY(location.getY() + 1);
-                } else if (location.getDirection() == Direction.SOUTH) {
-                    location.setY(location.getY() - 1);
-                } else if (location.getDirection() == Direction.EAST) {
-                    location.setX(location.getX() + 1);
-                } else if (location.getDirection() == Direction.WEST) {
-                    location.setX(location.getX() - 1);
-                }
+                move();
             } else if (givenCommand == Command.TURN_LEFT) {
-                if (location.getDirection() == Direction.NORTH) {
-                    location.setDirection(Direction.WEST);
-                } else if (location.getDirection() == Direction.SOUTH) {
-                    location.setDirection(Direction.EAST);
-                } else if (location.getDirection() == Direction.EAST) {
-                    location.setDirection(Direction.NORTH);
-                } else if (location.getDirection() == Direction.WEST) {
-                    location.setDirection(Direction.SOUTH);
-                }
+                turnLeft();
             } else if (givenCommand == Command.TURN_RIGHT) {
-                if (location.getDirection() == Direction.NORTH) {
-                    location.setDirection(Direction.EAST);
-                } else if (location.getDirection() == Direction.SOUTH) {
-                    location.setDirection(Direction.WEST);
-                } else if (location.getDirection() == Direction.EAST) {
-                    location.setDirection(Direction.SOUTH);
-                } else if (location.getDirection() == Direction.WEST) {
-                    location.setDirection(Direction.NORTH);
-                }
+                turnRight();
             }
+        }
+    }
+
+    private void move() {
+        if (location.getDirection() == Direction.NORTH) {
+            location.setY(location.getY() + 1);
+        } else if (location.getDirection() == Direction.SOUTH) {
+            location.setY(location.getY() - 1);
+        } else if (location.getDirection() == Direction.EAST) {
+            location.setX(location.getX() + 1);
+        } else if (location.getDirection() == Direction.WEST) {
+            location.setX(location.getX() - 1);
+        }
+    }
+
+    private void turnLeft() {
+        if (location.getDirection() == Direction.NORTH) {
+            location.setDirection(Direction.WEST);
+        } else if (location.getDirection() == Direction.SOUTH) {
+            location.setDirection(Direction.EAST);
+        } else if (location.getDirection() == Direction.EAST) {
+            location.setDirection(Direction.NORTH);
+        } else if (location.getDirection() == Direction.WEST) {
+            location.setDirection(Direction.SOUTH);
+        }
+    }
+
+    private void turnRight() {
+        if (location.getDirection() == Direction.NORTH) {
+            location.setDirection(Direction.EAST);
+        } else if (location.getDirection() == Direction.SOUTH) {
+            location.setDirection(Direction.WEST);
+        } else if (location.getDirection() == Direction.EAST) {
+            location.setDirection(Direction.SOUTH);
+        } else if (location.getDirection() == Direction.WEST) {
+            location.setDirection(Direction.NORTH);
         }
     }
 
