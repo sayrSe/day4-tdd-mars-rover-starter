e8ca04ef55453bc2d35aa83ca13269b9418151b8
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-17 21:04:54 +0800

    test: create test case change direction from South to East when turn left

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -64,4 +64,19 @@ class MarsRoverTest {
         assertEquals(-1 , currentLocation.getY());
         assertEquals(Direction.SOUTH , currentLocation.getDirection());
     }
+
+    @Test
+    void should_change_to_location_0_0_E_when_executeCommand_given_0_0_South_and_command_Turn_Left() {
+        // Given
+        Location initialLocation = new Location(0, 0, Direction.SOUTH);
+        Command givenCommand = Command.TURN_LEFT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+        // When
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+        // Then
+        assertEquals(0 , currentLocation.getX());
+        assertEquals(0 , currentLocation.getY());
+        assertEquals(Direction.EAST , currentLocation.getDirection());
+    }
 }
